feef4b0382b76c16c4ba0b1bd9c850b58d1a32c7
wb-wanghao <wb-wanghao@taobao.com>
2012-07-11 16:23:41 +0800

    first commit

--- data-stat/page.js
+++ data-stat/page.js
@@ -9,9 +9,27 @@ var S = KISSY, D = S.DOM, E = S.Event;
         //console.log(stat);
         if(stat){
             right += 1;
+            if(right === 1){
+                e.fire('click');
+                console.log('fire');
+                console.log(e);
+                var a1 = S.one('#J_Discover a'); 
+                a1.fire('click');
+                console.log(a1);
+            }
+            D.attr(e, 'title', stat);
+            D.css(e, {'color': 'rgba(0,0,0,0.5)', 'box-shadow': '#72BC00 0 0 3px 5px', 'background-color': 'rgba(0,0,0,0.4)'});
+            //if(D.css(e, 'position') != 'absolute') {
+            //    D.css(e, {'position':'relative'});
+            //}
+            //D.attr(e, 'edata-no', right);
+            //var text = D.html(e);
+            //if(!text){
+            //    D.text(e, '@');
+            //}
         }else {
             error += 1;
-            //D.css(e, {'position':'relative', 'color': 'rgba(0,0,0,0.5)', 'box-shadow': '#f44 0 0 3px 5px', 'background-color': 'rgba(0,0,0,0.4)'});
+            D.attr(e, 'title', 'no data-stat');
             D.css(e, {'color': 'rgba(0,0,0,0.5)', 'box-shadow': '#f44 0 0 3px 5px', 'background-color': 'rgba(0,0,0,0.4)'});
             if(D.css(e, 'position') != 'absolute') {
                 D.css(e, {'position':'relative'});
