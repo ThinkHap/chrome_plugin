b566884a2c28477f41dffb109cb2a1d52187a0ab
wanghao <wb-wanghao@taobao.com>
2012-11-22 11:57:13 +0800

    add 键盘事件

--- forbiddenUserTool/content.js
+++ forbiddenUserTool/content.js
@@ -1,6 +1,6 @@
 function KeyPress(){
     var key = event.keyCode;
-    if(key == 115) {// key on F4
+    if(key == 115 || key == 87 || key == 46) {// key on F4(115), W(87) , Delete(46)
         var ta=document.createElement("script");
         ta.innerText="var iframe = window.frames[0]; var form = iframe.document.getElementById('cpform'); var input1 = form.getElementsByTagName('tr')[4].getElementsByTagName('input')[1]; var input2 = form.getElementsByTagName('tr')[8].getElementsByTagName('input')[0]; var submit = form.getElementsByTagName('tr')[11].getElementsByTagName('input')[0]; input1.click(); input2.click(); setTimeout(submit.click(), 200)";
         document.getElementsByTagName("body")[0].appendChild(ta);
